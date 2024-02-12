922d3ff339d97fd7a92d0502d710a42c31a3b977
rafael rotiroti <rotirotirafa@gmail.com>
2024-02-12 15:22:47 -0300

    [Studying] Main

---
+++ src/src/Main.java
@@ -0,0 +1,27 @@
+import content.Variables;
+
+public class Main {
+    public static void main(String[] args) {
+        //type name = value;
+        int age;
+        age = 28;
+
+
+
+        //var name = value; defined scope;
+        var name = "Rafael";
+        var surname = " Rotiroti";
+
+        Variables variables = new Variables();
+
+        String printName = variables.fullName(name, surname);
+        System.out.println(printName);
+
+        Integer printAge = variables.age(age);
+        System.out.println(printAge);
+
+        String printYearBorn = variables.YearOfBirthDate(age);
+        System.out.println(printYearBorn);
+
+    }
+}
